5b4071bd5b1f452c0468aca5ff638af4c0d243fb
Javier M. Duarte <jduarte@ucsd.edu>
2020-11-08 00:38:20 +0000

    update

--- demos/graph_data.py
+++ demos/graph_data.py
@@ -10,9 +10,7 @@ import energyflow as ef
 ONE_HUNDRED_GEV = 100.0
 
 class GraphDataset(Dataset):
-    def __init__(self, root, transform=None, pre_transform=None, start=0, stop=-1):
-        self.start = start
-        self.stop = stop
+    def __init__(self, root, transform=None, pre_transform=None):
         super(GraphDataset, self).__init__(root, transform, pre_transform) 
 
 
@@ -23,11 +21,7 @@ class GraphDataset(Dataset):
     @property
     def processed_file_names(self):
         njetpairs = 10000
-        if self.start!=0 and self.stop!=-1:
-            njetpairs = self.stop-self.start
-            return ['data_{}.pt'.format(i) for i in range(self.start,self.stop)]
-        else:
-            return ['data_{}.pt'.format(i) for i in range(njetpairs)]
+        return ['data_{}.pt'.format(i) for i in range(njetpairs)]
 
     def __len__(self):
         return len(self.processed_file_names)
@@ -39,42 +33,41 @@ class GraphDataset(Dataset):
 
     def process(self):
         Js = []
-        num = 50
+        num = 100
         R = 0.4
         for raw_path in self.raw_paths:
             # load quark and gluon jets
             X, y = ef.qg_jets.load(200, pad=False, cache_dir=self.root+'/raw')
             # the jet radius for these jets
             # process jets
-            Gs, Qs = [], []
-            for arr,events in [(Gs, X[y==0]), (Qs, X[y==1])]:
-                for i,x in enumerate(events): 
-                    if i >= num: break
-                    # ignore padded particles and removed particle id information
-                    x = x[x[:,0] > 0,:3]
-                    # center jet according to pt-centroid
-                    yphi_avg = np.average(x[:,1:3], weights=x[:,0], axis=0)
-                    x[:,1:3] -= yphi_avg
-                    # mask out any particles farther than R=0.4 away from center (rare)
-                    x = x[np.linalg.norm(x[:,1:3], axis=1) <= R]
-                    # add to list
-                    arr.append(x)
-            Js.extend(Gs)
-            Js.extend(Qs)
-        jetpairs = [[i, j] for (i, j) in itertools.product(range(2*num),range(2*num))]
+            Js = []
+            for i,x in enumerate(X): 
+                if i >= num: break
+                # ignore padded particles and removed particle id information
+                x = x[x[:,0] > 0,:3]
+                # center jet according to pt-centroid
+                yphi_avg = np.average(x[:,1:3], weights=x[:,0], axis=0)
+                x[:,1:3] -= yphi_avg
+                # mask out any particles farther than R=0.4 away from center (rare)
+                x = x[np.linalg.norm(x[:,1:3], axis=1) <= R]
+                # add to list
+                Js.append(x)
+        jetpairs = [[i, j] for (i, j) in itertools.product(range(num),range(num))]
         for k, (i, j) in enumerate(jetpairs):
             emdval = ef.emd.emd(Js[i], Js[j], R=R)/ONE_HUNDRED_GEV
-            jetpair = np.concatenate([Js[i], Js[j]], axis=0)
             Ei = np.sum(Js[i][:,0])
-            Js[i][:,0] = Js[i][:,0]/Ei
             Ej = np.sum(Js[j][:,0])
-            Js[j][:,0] = Js[j][:,0]/Ej
+            jiNorm = Js[i].copy()
+            jjNorm = Js[j].copy()
+            jiNorm[:,0] = jiNorm[:,0]/Ei
+            jjNorm[:,0] = jjNorm[:,0]/Ej
+            jetpair = np.concatenate([jiNorm, jjNorm], axis=0)
             nparticles_i = len(Js[i])
             nparticles_j = len(Js[j])
             pairs = [[m, n] for (m, n) in itertools.product(range(0,nparticles_i),range(nparticles_i,nparticles_i+nparticles_j))]
             edge_index = torch.tensor(pairs, dtype=torch.long)
             edge_index=edge_index.t().contiguous()
-            u = torch.tensor([[Ei, Ej]], dtype=torch.float)
+            u = torch.tensor([[Ei/ONE_HUNDRED_GEV, Ej/ONE_HUNDRED_GEV]], dtype=torch.float)
             x = torch.tensor(jetpair, dtype=torch.float)
             y = torch.tensor([[emdval]], dtype=torch.float)
             data = Data(x=x, edge_index=edge_index, y=y, u=u)
